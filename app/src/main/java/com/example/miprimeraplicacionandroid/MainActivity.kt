581666ef6f7de59a7c2274a9d52457f6a7522c7b
unknown <randy97_dx@hotmail.com>
2021-02-25 16:43:19 -0600

    tarea

---
+++ app/src/main/java/com/example/miprimeraplicacionandroid/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.miprimeraplicacionandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
